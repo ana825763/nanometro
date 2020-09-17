424d25d5b853ae6bcd248fff7c9ef4be47bcfca2
MFerreira3 <goodnews.room@hotmail.com>
2020-09-17 15:06:40 -0300

    Cospe ainda mais fatos

--- main.lua
+++ main.lua
@@ -1,3 +1,3 @@
 truth = 73
 
-print(string.char(truth) .. string.char(truth+3) .. string.char(truth+16))
+print(string.char(truth) .. string.char(truth+3) .. string.char(truth+16) .. string.char(truth-41) .. utf8.char(10084))
